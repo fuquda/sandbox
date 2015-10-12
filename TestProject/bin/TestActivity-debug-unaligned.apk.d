adfba928a02b8399b960eecf0e7bc37a68ab8dd7
Keita.Fukuda <fuquda@gmail.com>
2015-10-13 00:05:04 +0900

    Delete TestProject.

--- TestProject/bin/TestActivity-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-/home/fukuda/work/adt-bundle-linux-x86_64-20140702/workspace/TestProject/bin/TestActivity-debug-unaligned.apk : \
-/home/fukuda/work/adt-bundle-linux-x86_64-20140702/workspace/TestProject/bin/TestActivity.ap_ \
-/home/fukuda/work/adt-bundle-linux-x86_64-20140702/workspace/TestProject/bin/classes.dex \
